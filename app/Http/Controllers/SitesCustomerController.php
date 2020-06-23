dd08843f6e2ef718b0ada3e7161a708ce372d85b
Dayaris Rojas <dayaris.rojas2014@gmail.com>
2020-06-23 13:40:40 -0500

    First commit

---
+++ app/Http/Controllers/SitesCustomerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SitesCustomer;
+use Illuminate\Http\Request;
+
+class SitesCustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SitesCustomer  $sitesCustomer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SitesCustomer $sitesCustomer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SitesCustomer  $sitesCustomer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SitesCustomer $sitesCustomer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SitesCustomer  $sitesCustomer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SitesCustomer $sitesCustomer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SitesCustomer  $sitesCustomer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SitesCustomer $sitesCustomer)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResources(['catalogs'=> 'CatalogController']);
+
+/*
+Route::group(['prefix' => 'jobs'], function (){
+    Route::post('postulations', 'JobsController@viewPostulations');
+    Route::get('/', 'JobsController@getAll');
+    Route::post('/','JobsController@store');
+    Route::get('{id}','JobsController@getById');
+    Route::post('manage/search', 'JobsController@searchForJob');
+    Route::post('postulate', 'JobsController@postulate');
+    Route::PUT('postulations/select', 'JobsController@selectCandidate');
+    Route::put('change/status/{id}', 'JobsController@updateStatusForJob');
+    Route::put('assigncandidate/{id}', 'JobsController@assignCandidate');
+});*/
+
+
