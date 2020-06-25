d35d15c04a797bccc573c8c77cbfad68dec7a160
dayarisrt <dayaris.rojas2014@gmail.com>
2020-06-25 00:00:08 -0500

    Add methods in sitescustomer and collection

--- app/Http/Controllers/SitesCustomerController.php
+++ app/Http/Controllers/SitesCustomerController.php
@@ -14,7 +14,8 @@ class SitesCustomerController extends Controller
      */
     public function index()
     {
-        //
+        $sites = SitesCustomer::All()->toArray(); 
+        return response()->json($sites,200);
     }
 
     /**
@@ -35,7 +36,16 @@ class SitesCustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $site = SitesCustomer::where('name','like', '%'.strtolower($request->name).'%');
+        if($site->count()){
+            return response()->json(['data' => $site->first(), 'msj' => 'Ya existe un sitio con este nombre!'],200);
+        }
+        $site = new SitesCustomer;
+        $site->fill($request->all());
+        
+        if($site->save()){
+            return response()->json(['data' => $site->first(), 'msj' => 'Sitio registrado exitosamente!'],200);
+        }
     }
 
     /**
@@ -46,7 +56,7 @@ class SitesCustomerController extends Controller
      */
     public function show(SitesCustomer $sitesCustomer)
     {
-        //
+        return response()->json($sitesCustomer,200);
     }
 
     /**
@@ -69,7 +79,13 @@ class SitesCustomerController extends Controller
      */
     public function update(Request $request, SitesCustomer $sitesCustomer)
     {
-        //
+        $sitesCustomer->fill($request->all());
+        
+        if($sitesCustomer->save()){
+            return response()->json(['data' => $sitesCustomer, 'msj' => 'Sitio modificado exitosamente!'],200);
+        }else{
+            return response()->json(['data' => $sitesCustomer, 'msj' => 'No existe el sitio indicado'],200);
+        }
     }
 
     /**
@@ -80,6 +96,18 @@ class SitesCustomerController extends Controller
      */
     public function destroy(SitesCustomer $sitesCustomer)
     {
-        //
+        if($sitesCustomer->active==true){
+            $sitesCustomer->active=false;
+            $status='desactiva';
+        }else{
+            $sitesCustomer->active=true;
+            $status='activa';
+        }
+        
+        if($sitesCustomer->save()){
+            return response()->json(['data' => $sitesCustomer, 'msj' => 'Sitio '.$status.'do exitosamente!'],200);
+        }else{
+            return response()->json(['data' => $sitesCustomer, 'msj' => 'No se pudo '.$status.'r el sitio indicado'],200);
+        }
     }
 }
